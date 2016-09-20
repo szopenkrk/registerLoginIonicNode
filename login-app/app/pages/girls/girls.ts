730b2c4d8c4529791000cbb9aabf48deeca8824e
Karol Kochanski <karol.kochanski@sensinum.pl>
2016-09-20 17:10:04 +0200

    Up

--- login-app/app/pages/girls/girls.ts
+++ login-app/app/pages/girls/girls.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 /*
-  Generated class for the TabsPage page.
+  Generated class for the GirlsPage page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
 @Component({
-  templateUrl: 'build/pages/tabs/tabs.html',
+  templateUrl: 'build/pages/girls/girls.html',
 })
-export class TabsPage {
+export class GirlsPage {
 
   constructor(private navCtrl: NavController) {
 
--- login-app/app/pages/map/map.ts
+++ login-app/app/pages/map/map.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 /*
-  Generated class for the TabsPage page.
+  Generated class for the MapPage page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
 @Component({
-  templateUrl: 'build/pages/tabs/tabs.html',
+  templateUrl: 'build/pages/map/map.html',
 })
-export class TabsPage {
+export class MapPage {
 
   constructor(private navCtrl: NavController) {
 
