ae05151a6dbf79c81ebd5655bdeec92a4e74a0cb
Karol Kochanski <karol.kochanski@sensinum.pl>
2016-09-20 15:34:19 +0200

    Update

--- login-app/www/js/controllers.js
+++ login-app/www/js/controllers.js
@@ -18,11 +18,18 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('LoginCtrl', function($scope) {
+.controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
   $scope.data = {};
 
   $scope.login = function() {
-    console.log("LOGIN user: " + $scope.data.username + " - PW: " + $scope.data.password);
+    LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+      $state.go('tab.dash');
+    }).error(function(data) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Login failed!',
+        template: 'Please check your credentials!'
+      });
+    });
   }
 })
 
